fd184fb218a57cd14733420d9c64871c98146b94
ASLLEY G. GLARAGA <2302803@cc.usls.edu>
2025-04-30 16:17:41 +0800

    Added accessors and mutators to Pet class

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -1,31 +1,25 @@
-package com.ecpe308.sample;
-
 public class Pet {
-    public String name, breed; //persian, labrador, golden retriever
-    public String pet_type;  //cat/dog
+    private String name;
+    private String type;
+    
+    public Pet(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getBreed(){
-        return breed;
+    public String getType() {
+        return type;
     }
 
-    public String getPet_type(){
-        return pet_type;
-    }
-
-    public void setName (String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setBreed (String breed){
-        this.breed = breed; 
-    }
-    
-    public void setPet_type (String pet_type){
-        this.pet_type= pet_type;
+    public void setType(String type) {
+        this.type = type;
     }
 }
-
