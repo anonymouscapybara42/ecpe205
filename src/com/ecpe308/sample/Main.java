f94f9f1072658e74cf46ca2cf4f349dcd5e069ab
ASLLEY G. GLARAGA <2302803@cc.usls.edu>
2025-04-30 16:25:35 +0800

    changed the text

--- src/com/ecpe308/sample/Main.java
+++ src/com/ecpe308/sample/Main.java
@@ -2,7 +2,7 @@ package com.ecpe308.sample;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Mathieu van der Poel is the strongest Cyclist in the world");
     }
 }
 
