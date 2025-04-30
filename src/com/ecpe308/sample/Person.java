71e35d61e9d795d9254882b6fc00ea23a0dd7bc0
ASLLEY G. GLARAGA <2302803@cc.usls.edu>
2025-04-30 16:34:17 +0800

    updated code

--- src/com/ecpe308/sample/Person.java
+++ src/com/ecpe308/sample/Person.java
@@ -1,12 +1,15 @@
 package com.ecpe308.sample;
 
+import java.util.Objects;
+
 public class Person {
-    private String lastName, firstName, address;
+    private String lastName;
+    private String firstName;
+    private String address;
     private int age;
-
+    
     public Person() {
-
-    };
+    }
 
     public Person(String lastName, String firstName, String address, int age) {
         this.lastName = lastName;
@@ -14,6 +17,7 @@ public class Person {
         this.address = address;
         this.age = age;
     }
+
 
     public String getLastName() {
         return lastName;
@@ -45,5 +49,27 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person[firstName=%s, lastName=%s, age=%d, address=%s]",
+                firstName, lastName, age, address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(firstName, person.firstName) &&
+                Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, firstName, address, age);
     }
 }
